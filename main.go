d75f98011896f7fffb17ad226c76dfab5b7368e8
mahdihp.devsc <messenger123456>
2021-12-01 19:56:10 +0330

    Init Git 1400-09-10

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	config2 "SuperPanel/config"
 	"SuperPanel/models"
+	"fmt"
+	"github.com/spf13/viper"
 	mysql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"strconv"
 )
 
+var config config2.Configurations
+
+func init() {
+	InitConfig()
+}
 func main() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/super_panel?charset=utf8&parseTime=True&loc=Local"
+	db := InitDb()
+	fmt.Println(db.Config.Name())
+}
+
+func InitDb() *gorm.DB {
+	dsn := config.Database.DBUser + ":" +
+		config.Database.DBPassword + "@tcp(127.0.0.1:" +
+		strconv.Itoa(config.Database.DBPort) + ")/" + config.Database.DBName +
+		"?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -17,6 +34,23 @@ func main() {
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Role{})
 	db.AutoMigrate(&models.Permission{})
-	//db.Create(&models.User{ FirstName: "mahdi",LastName: "hp"})
+	return db
+}
 
+func InitConfig() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+	viper.SetDefault("database.dbname", "super_panel")
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	fmt.Println("Database is\t", config)
 }
